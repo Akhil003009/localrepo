b08da61e460daa8ab20a6025c237bc78fa2e173b
Akhil003009 <Akhilkr097@gmail.com>
2024-05-24 17:15:41 +0530

    add a java file

---
+++ Static1.java
@@ -0,0 +1,29 @@
+package LocalRepo;
+
+public class Static1 {
+
+
+    int x = 500; //defining a variable
+    String s = "Selenium"; //defining a variable
+    static int y = 100; // defining a static int variable
+
+
+    public static void main(String[] args) {
+    
+        String x = "Hello"; //defining the local variable
+
+        System.out.println(x); // Calling x variable
+
+    
+    }
+
+ 
+
+
+
+}
+
+
+
+
+
